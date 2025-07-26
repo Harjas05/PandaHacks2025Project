fdbcaee00d25cdf485fda854e98ceb686b81e507
Harjas05 <harjas@umich.edu>
2025-07-26 17:38:56 -0400

    upated pseudocode and loading api key

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,22 @@
+import streamlit as st
+import os 
+from dotenv import load_dotenv
+
+# load apikey safely from.env file
+load_dotenv()
+API_KEY = os.getenv("HF_TOKEN")
+
+#get the user input from the front-end 
+#store it and then pass it off to the llm
+# sanitize?
+#Construct prompt for LLM
+# Call Hugging Face model using Inference API
+# Return structured response to frontend
+
+
+
+
+
 #prompt the user for symptoms
 # repeat until user ends chat
 # store symptoms
---
+++ frontend/hf.py
@@ -0,0 +1,5 @@
+
+
+#prompt user for input
+# get the input and give it to th backend
+# get it from the backend and then display it
